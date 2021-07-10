bf75da6adc97877c700cc0105ff3d307846dd57d
Ayushj174 <ayushj174@gmail.com>
2021-07-10 16:21:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 az artifacts universal publish --organization https://dev.azure.com/ajain997101/
     --project="ayush-learning"
-    --scope project --feed ayush-artifact --name '*' --version 0.0.1 --description "Welcome to Universal Packages" --path '$(build.artifactsstagingdirectory)'
+    --scope project --feed ayush-artifact --name '*' --version 0.0.1 --description "Welcome to Universal Packages" --path '$(System.DefaultWorkingDirectory)'
